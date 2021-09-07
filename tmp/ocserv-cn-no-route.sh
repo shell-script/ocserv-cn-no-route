15361d1fbcae49b6d754301b375f562391b6b94c
CNMan <nklijl@mail.nankai.edu.cn>
2021-09-08 00:11:55 +0800

    Wed Sep  8 00:11:55 HKT 2021

--- tmp/ocserv-cn-no-route.sh
+++ tmp/ocserv-cn-no-route.sh
@@ -1,7 +1,7 @@
 cd /root/githubrepo/ocserv-cn-no-route/tmp
 wget -N https://gitlab.com/ocserv/ocserv/raw/master/doc/sample.config
 
-python ocserv-cn-no-route.py
+python3 ocserv-cn-no-route.py
 
 cd /root/githubrepo/ocserv-cn-no-route
 git add .
